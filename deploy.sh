ce7dbea4ec57ab592c4ca6c5948f7f741025daa8
Matt West <matt.west@developercity.co.uk>
2012-07-24 21:38:20 +0100

    Added new post "The Golden Circle"

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Attempting to deploy..."
-ssh root@46.32.255.68 'cd /var/www/vhosts/codingskyscrapers.com/httpdocs/coding-skyscrapers; git pull; go build; ./coding-skyscrapers stop; ./coding-skyscrapers start&'
+ssh root@46.32.255.68 'cd /var/www/vhosts/codingskyscrapers.com/httpdocs/coding-skyscrapers; git pull; go build; ./coding-skyscrapers restart&'
