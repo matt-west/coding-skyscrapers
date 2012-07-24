9838e723fe77ba80856520fe5c2e882a2d9863e6
Matt West <matt.west@developercity.co.uk>
2012-07-24 20:53:06 +0100

    More work on deploy script (almost done)

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-cd /var/www/vhosts/codingskyscrapers.com/httpdocs/coding-skyscrapers
-echo "Pulling code..."
-git pull
-echo "Building..."
-go build
-echo "Stopping Server..."
-./coding-skyscrapers stop
-echo "Starting Server..."
-./coding-skyscrapers start&
+echo "Attempting to deploy..."
+ssh root@46.32.255.68 'cd /var/www/vhosts/codingskyscrapers.com/httpdocs/coding-skyscrapers; git pull; go build; ./coding-skyscrapers stop; ./coding-skyscrapers start&'
